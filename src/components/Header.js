399fc10494bead3e9592825815ed4a4aa1670588
laiba-ned <ali4403321@cloud.neduet.edu.pk>
2024-12-22 14:55:57 +0500

    added some components

---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Button from '../sharedComponents/Button'
+
+export default function Header() {
+  return (
+    <div>
+        <h1>Header Component</h1> 
+        <Button/>
+    </div>
+  )
+}
