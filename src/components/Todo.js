b16c0806d24b84243d6c7d30ffc0a6a04bb237a8
tayyaba-ali <ta6402792@gmail.com>
2025-01-12 15:05:21 +0500

    useMemo and useCallback hooks

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,70 +1,40 @@
-import React, { useReducer } from 'react'
+import React, {useState, useMemo, useCallback} from 'react'
+import Todosss from './Todosss';
 
-const todoList = [
-    {
-        id: 1,
-        title: "Read Book",
-        complete: false,
-      },
-      {
-        id: 2,
-        title: "Play Football",
-        complete: false,
-      },
-      {
-        id: 3,
-        title: "Pray",
-        complete: false,
-      },
-      {
-        id: 4,
-        title: "Have Lunch",
-        complete: false,
-      }];
-
-function reducer(state, action) {
-    switch(action.type) {
-        case "Complete":
-            console.log('Task Done...', action.id)
-            return state.map(todo => {
-                if (todo.id === action.id) {
-                    return {...todo, complete: !todo.complete}
-                }
-                else {
-                    return todo
-                }
-            })
-        default: 
-            console.log('Default action...')
-            return state
+const expensiveCalculation = (num) => {
+    console.log("Calculating...");
+    for (let i = 0; i < 1000000000; i++) {
+      num += 1;
     }
-}
+    return num;
+  };
 
 function Todo() {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+  const calculation = useMemo(() => expensiveCalculation(count), [count]);
 
-    const [todos, dispatch] = useReducer(reducer, todoList)
-    console.log(todos)
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
 
-    const handleChange = (todo) => {
-        dispatch({type: "Complete", id: todo.id})
-    }
+  const addTodo = useCallback(() => {
+    console.log('add todo...')
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos]);
+
   return (
-    <>
-        <h1>Todo App</h1>
-        {
-            todos.map(todo => (
-                <div key={todo.id}>
-                    <span style={{display: 'flex'}}>
-                        <h3>{todo.title}</h3>
-                    {/* { !todo.complete &&  */}
-                        <button onClick={() => handleChange(todo)}>Done</button>
-        {/* } */}
-                    </span>
-                </div>
-            ))
-        }
-    </>
-  )
+    <div>
+      <Todosss todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+        <h2>Expensive Calculation</h2>
+        {calculation}
+      </div>
+    </div>
+  );    
 }
 
 export default Todo
